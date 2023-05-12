b0f167e381a4b0dcc51a4a41d4d1e2c7749f0240
DevSuelen <suelenbarros12008@gmail.com>
2023-05-11 21:45:16 -0300

    resolvendo exercicios com condicionais

--- aula-02/condicional.js
+++ aula-02/condicional.js
@@ -1,9 +1,30 @@
-let numero = 11
+/* let numero = 10
 
 if (numero < 10) {
 console.log('é menor que 10')
 
+} else  if (numero > 10){
+console.log('é maior que 10')
+    
 } else {
-    console.log('É maior que 10')
+    console.log('É igual a 10')
+} */
 
+
+const comparacao10 = (numero) => {
+    if (numero < 10) {
+        console.log('é menor que 10')
+        
+        } else  if (numero > 10){
+        console.log('é maior que 10')
+            
+        } else {
+            console.log('É igual a 10')
+        }
 }
+
+comparacao10(2)
+comparacao10(80)
+comparacao10(10)
+
+
---
+++ aula-02/exercicio.js
@@ -0,0 +1,15 @@
+const comparacao50 = (numero) =>{
+    if(numero<50){
+        console.log('é menor que 50')
+    }else if(numero > 50){
+        console.log('émaior que 50')
+    } else{
+        console.log('é igual a 50')
+    }
+
+
+}
+
+comparacao50(20,30)
+comparacao50(40,50)
+comparacao50(50)
