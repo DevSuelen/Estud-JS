c4749031bddb763865c42cb811c6562e0e8f3e1c
DevSuelen <suelenbarros12008@gmail.com>
2023-05-11 21:15:12 -0300

    estudando condicional if e else

---
+++ aula-02/arrow_function.js
@@ -0,0 +1,12 @@
+const soma = (n1, n2) => {
+    let resultado = (n1+ n2)
+    console.log('soma:', resultado)
+}
+//soma(10, 10)
+
+const soma2 = (n1, n2) => n1 + n2
+console.log(soma2(10,20))
+
+const media2 =(n1, n2) => soma2(n1,n2)/2
+console.log(media2(30,50))
+
---
+++ aula-02/condicional.js
@@ -0,0 +1,9 @@
+let numero = 11
+
+if (numero < 10) {
+console.log('é menor que 10')
+
+} else {
+    console.log('É maior que 10')
+
+}
